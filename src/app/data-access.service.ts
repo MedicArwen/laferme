8a66d854aa793ab53e8174fe3193fe9de01f55fc
MedicArwen <45285123+MedicArwen@users.noreply.github.com>
2021-07-01 16:04:01 +0200

    Update data-access.service.ts
    mak

--- src/app/data-access.service.ts
+++ src/app/data-access.service.ts
@@ -1,13 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Animal } from './models/animal.model';
+import { AnimalsService } from './services/animals.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataAccessService {
 
-  constructor(private httpService:HttpClient)
+  constructor(private httpService:HttpClient,private animalService:AnimalsService)
    { 
      
    }
@@ -18,5 +20,13 @@ export class DataAccessService {
         )
         .subscribe((res:any) => {console.log(res); });
   }
+  fetchData()
+  {
+this.httpService.get<Animal[]>('https://heroacademy-72e10-default-rtdb.europe-west1.firebasedatabase.app/animals.json')
+    .pipe(tap((animalList: Animal[]) => {
+        if (animalList!=null)
+      	this.animalService.setListe(animalList);
+      }) ) .subscribe();
+ }
 
 }
